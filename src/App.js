221e4a6db90d8500b66a9d259c1bab6ef92b8c99
nguyendayIT <nguyendaygit@gmail.com>
2022-12-19 13:27:29 +0700

    first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Mycomponent from './components/Mycomponent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
+    <div> 
+      
+      <div style={{color: 'red', fontSize: 70 , marginBottom:50}}>Day Nguyen</div>
+     
+      <Mycomponent/>
     </div>
   );
 }
---
+++ src/components/Mycomponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Mycomponent extends Component {
+   // JSX
+    render() {
+        return (
+            <div>
+                My first component cua Day Nguyen
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+
+export default Mycomponent;
